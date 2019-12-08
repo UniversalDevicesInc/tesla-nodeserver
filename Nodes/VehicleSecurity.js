9cbaf455eab5523f8f2f936aac65a98f4301ea34
Blake Von Haden <6832052+blakevh@users.noreply.github.com>
2019-12-08 15:01:35 -0600

    Trying to figure out what type the value is

--- Nodes/VehicleSecurity.js
+++ Nodes/VehicleSecurity.js
@@ -155,7 +155,7 @@ module.exports = function(Polyglot) {
     async onSentryMode(message) {
       const id = this.vehicleId();
       const decodeValue = message.value === 1 ? 'on' : 'off';
-      logger.debug('SENTRY MODE raw %s decoded %s (%s)', message.value, decodeValue, this.address);
+      logger.debug('SENTRY MODE raw %s type %s decoded %s (%s)', message.value, message.constructor.name, decodeValue, this.address);
       await this.tesla.cmdSentryMode(id, decodeValue);
       await this.query();
     }
