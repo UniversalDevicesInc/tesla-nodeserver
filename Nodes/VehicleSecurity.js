0b94acacc6a259c249421ef72c90de60d9285106
Blake Von Haden <6832052+blakevh@users.noreply.github.com>
2019-12-15 17:20:23 -0600

    Trying to figure out sun roof display.

--- Nodes/VehicleSecurity.js
+++ Nodes/VehicleSecurity.js
@@ -286,7 +286,8 @@ module.exports = function(Polyglot) {
 
         this.setDriver('GV8', vehicleState.locked, false);
 
-       	this.setDriver('GV9', vehicleState.sun_roof_percent_open, false);
+        logger.debug('vehicleState.sun_roof_percent_open %s', vehicleState.sun_roof_percent_open);
+          this.setDriver('GV9', vehicleState.sun_roof_percent_open, false);
 
         // Status of sentry mode (displayed with an index).
         this.setDriver('GV11', vehicleState.sentry_mode ? 1 : 0, false);
