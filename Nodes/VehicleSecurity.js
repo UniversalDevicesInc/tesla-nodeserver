978e2162b4d0a8fa096cbabcaf8210f5b471ab04
Blake Von Haden <6832052+blakevh@users.noreply.github.com>
2020-01-19 12:11:20 -0600

    Switch level to warn.

--- Nodes/VehicleSecurity.js
+++ Nodes/VehicleSecurity.js
@@ -34,6 +34,7 @@ module.exports = function(Polyglot) {
       
       for (const transport of logger.transports) {
         logger.debug('VehicleSecurity() transports: %s', transport.level);
+        transport.level = 'warn';
       }
 
       // PGC supports setting the node hint when creating a node
